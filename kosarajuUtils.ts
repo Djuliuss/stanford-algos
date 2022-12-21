75ea828080f4ea9c25b07436e3f373430a64fc9a
Julio Delgado <juliods@gmail.com>
2022-12-21 12:55:13 +0000

    validateGraph

--- kosarajuUtils.ts
+++ kosarajuUtils.ts
@@ -1,4 +1,24 @@
 import { DirectedGraph } from "./kosarajuTypes";
+import { graph } from "./testData";
+
+const getTotalArcs = (graph: DirectedGraph) => {
+  const totalArcsByVortex = Object.values(graph).map((arcs) => arcs.length);
+  const totalArcs = totalArcsByVortex.reduce((acc, el) => acc + el);
+  return totalArcs;
+};
+
+const validateGraph = (gr1: DirectedGraph, gr2: DirectedGraph) => {
+  const verticesGr1 = Object.keys(gr1).length;
+  const verticesGr2 = Object.keys(gr2).length;
+  if (verticesGr1 !== verticesGr2) {
+    throw new Error(`invalid number vertices`);
+  }
+  const arcsGr1 = getTotalArcs(gr1);
+  const arcsGr2 = getTotalArcs(gr2);
+  if (arcsGr1 !== arcsGr2) {
+    throw new Error(`invalid number arcs`);
+  }
+};
 
 export const invertGraph = (dgraph: DirectedGraph) => {
   const intevertedGraph: DirectedGraph = {};
@@ -14,6 +34,7 @@ export const invertGraph = (dgraph: DirectedGraph) => {
         : (intevertedGraph[arc] = [numberVortex]);
     });
   });
+  validateGraph(graph, intevertedGraph);
   return intevertedGraph;
 };
 
