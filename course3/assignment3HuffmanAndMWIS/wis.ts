5e1297b29f1914b173ae191a697d8c79d9925524
Julio Delgado <juliods@gmail.com>
2023-01-19 12:32:22 +0000

    it works but it takes too long

--- course3/assignment3HuffmanAndMWIS/wis.ts
+++ course3/assignment3HuffmanAndMWIS/wis.ts
@@ -1,4 +1,21 @@
 import { vortex } from "./types";
+const nReadlines = require("n-readlines");
+
+export const calculateWmisFromFile = async (filename: string) => {
+  const vertices: vortex[] = [];
+  const lines = new nReadlines(filename);
+  const numberVertices = Number(lines.next().toString("ascii"));
+  let index = 1;
+  let line;
+  while ((line = lines.next())) {
+    vertices.push({
+      vortexNumber: index,
+      weight: Number(line.toString("ascii")),
+    });
+    index++;
+  }
+  return calculateWmis(vertices);
+};
 
 const calculateWmis = (vertices: vortex[]): vortex[] => {
   if ([0, 1].includes(vertices.length)) {
@@ -17,18 +34,20 @@ const calculateWmis = (vertices: vortex[]): vortex[] => {
 const totalWeight = (vertices: vortex[]) =>
   vertices.reduce((acc, { weight }) => (acc += weight), 0);
 
-const testData: vortex[] = [
-  { vortexNumber: 1, weight: 1 },
-  { vortexNumber: 2, weight: 4 },
-  { vortexNumber: 3, weight: 5 },
-  { vortexNumber: 4, weight: 4 },
-];
+// const testData: vortex[] = [
+//   { vortexNumber: 1, weight: 1 },
+//   { vortexNumber: 2, weight: 4 },
+//   { vortexNumber: 3, weight: 5 },
+//   { vortexNumber: 4, weight: 4 },
+// // ];
 
-const response = calculateWmis(testData);
-console.error(
-  `JD!!! wis.ts 30. The value of response is ${JSON.stringify(
-    response,
-    null,
-    2
-  )} `
-);
+// const response = calculateWmisFromFile(
+//   "/Users/julio/algorithms/course3/assignment3HuffmanAndMWIS/testCases/question3/input_random_1_10.txt"
+// );
+// console.error(
+//   `JD!!! wis.ts 30. The value of response is ${JSON.stringify(
+//     response,
+//     null,
+//     2
+//   )} `
+// );
---
+++ course3/assignment3HuffmanAndMWIS/wisTester.ts
@@ -0,0 +1,102 @@
+import { calculateWmisFromFile } from "./wis";
+import { vortex } from "./types";
+
+const nReadlines = require("n-readlines");
+const testFileNames = [
+  // "1_10",
+  // "2_10",
+  // "3_10",
+  // "4_10",
+  // "5_20",
+  // "6_20",
+  // "7_20",
+  // "8_20",
+  // "9_40",
+  // "10_40",
+  // "11_40",
+  // "12_40",
+  // "13_80",
+  // "14_80",
+  // "15_80",
+  // "16_80",
+  // "17_160",
+  // "18_160",
+  // "19_160",
+  // "20_160",
+  // "21_320",
+  // "22_320",
+  // "23_320",
+  // "24_320",
+  // "25_640",
+  // "26_640",
+  // "27_640",
+  // "28_640",
+  "29_1000",
+  "30_1000",
+  "31_1000",
+  "32_1000",
+  // "33_2000",
+  // "34_2000",
+  // "35_2000",
+  // "36_2000",
+  // "37_4000",
+  // "38_4000",
+  // "39_4000",
+  // "40_4000",
+  // "41_8000",
+  // "42_8000",
+  // "43_8000",
+  // "44_8000",
+  // "45_10000",
+  // "46_10000",
+  // "47_10000",
+  // "48_10000",
+];
+
+(async () => {
+  console.time("test");
+  let [error, success] = [0, 0];
+  for (const testFileName of testFileNames) {
+    const inputFile = `./course3/assignment3HuffmanAndMWIS/testCases/question3/input_random_${testFileName}.txt`;
+    const outputFile = `./course3/assignment3HuffmanAndMWIS/testCases/question3/output_random_${testFileName}.txt`;
+    const response = await calculateWmisFromFile(inputFile);
+    const expectedResult = await fetchExpectedResult(outputFile);
+    if (workOutResponse(response) !== expectedResult) {
+      console.error(
+        `maximumbites test ${testFileName} failed response for test1  ${response} expectedResult ${expectedResult}`
+      );
+      error++;
+    } else {
+      console.info(`SUCCESSFUL TEST !!! ${testFileName}`);
+      success++;
+    }
+  }
+  console.info(`RESULTS OF TESTS SUCCESS: ${success} ERRORS: ${error}`);
+  console.timeEnd("test");
+})();
+
+function workOutResponse(vertices: vortex[]) {
+  let response = "";
+  const verticesToCheck = [1, 2, 3, 4, 17, 117, 517, 997];
+  verticesToCheck.forEach((vortex) => {
+    if (isVortexInResponse(vortex, vertices)) {
+      response += "1";
+    } else {
+      response += "0";
+    }
+  });
+  return response;
+}
+
+const isVortexInResponse = (vortexNumberToFind: number, vertices: vortex[]) => {
+  const found = vertices.find(
+    ({ vortexNumber }) => vortexNumber === vortexNumberToFind
+  );
+  return !!found;
+};
+
+export async function fetchExpectedResult(filename: string) {
+  const broadbandLines = new nReadlines(filename);
+  let line = broadbandLines.next();
+  return line.toString("ascii");
+}
