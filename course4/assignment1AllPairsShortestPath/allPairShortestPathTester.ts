58729de31701b860a77c96bd202594f77143eb9c
Julio Delgado <juliods@gmail.com>
2023-01-30 12:22:00 +0000

    simple tests pass

--- course4/assignment1AllPairsShortestPath/allPairShortestPathTester.ts
+++ course4/assignment1AllPairsShortestPath/allPairShortestPathTester.ts
@@ -1,7 +1,8 @@
+import { identicalArrays } from "../../course2/dijkstra/utils";
 import { floydWarshall } from "./allPairsShortestPatheeee";
 import { Graph } from "./types";
 
-const inputData1: Graph = {
+const testData1: Graph = {
   1: [
     { head: 2, length: 2 },
     { head: 3, length: 5 },
@@ -11,11 +12,41 @@ const inputData1: Graph = {
   4: [{ head: 1, length: 4 }],
 };
 
-const response = floydWarshall(inputData1);
-console.error(
-  `JD!!! allPairShortestPathTester.ts 15. The value of response is ${JSON.stringify(
-    response,
-    null,
-    2
-  )} `
-);
+const expectedResponse1 = [
+  [0, 2, 3, 6],
+  [8, 0, 1, 4],
+  [7, 9, 0, 3],
+  [4, 6, 7, 0],
+];
+
+const testData2: Graph = {
+  1: [
+    { head: 2, length: 2 },
+    { head: 3, length: 5 },
+  ],
+  2: [{ head: 3, length: 1 }],
+  3: [{ head: 4, length: -3 }],
+  4: [{ head: 1, length: -4 }],
+};
+
+const expectedResponse2 = [
+  [-4, -2, -1, -4],
+  [-6, -4, -3, -6],
+  [-7, -5, -4, -7],
+  [-8, -6, -5, -8],
+];
+
+const testSet: Array<{ testData: Graph; expectedResponse: number[][] }> = [
+  { testData: testData1, expectedResponse: expectedResponse1 },
+  { testData: testData2, expectedResponse: expectedResponse2 },
+];
+
+testSet.forEach(({ testData, expectedResponse }) => {
+  const response = floydWarshall(testData);
+  response.forEach((_, index) => {
+    if (!identicalArrays(expectedResponse[index], response[index])) {
+      throw new Error(`test failed`);
+    }
+  });
+});
+console.info(`TESTS SUCCESSFUL`);
--- course4/assignment1AllPairsShortestPath/utils.ts
+++ course4/assignment1AllPairsShortestPath/utils.ts
@@ -9,3 +9,16 @@ export const getLengthBetweenVertices = (
   const edge = edges.find(({ head: headEdge }) => headEdge === head);
   return edge?.length ?? INFINITY;
 };
+
+export const identicalArrays = (arr1: any[], arr2: any[]) => {
+  let response = true;
+  if (arr1.length !== arr2.length) {
+    response = false;
+  }
+  arr1.forEach((el, index) => {
+    if (arr2[index] !== el) {
+      response = false;
+    }
+  });
+  return response;
+};
